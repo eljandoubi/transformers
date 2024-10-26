7582140b4c5f2505e39eebb3d4047521d79737fc
eljandoubi <abdelkarim.eljandoubi@ensta-paris.fr>
2024-10-26 17:25:05 +0200

    see ds zero init

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1536,6 +1536,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
             logger.info("Detected DeepSpeed ZeRO-3: activating zero.init() for this model")
             # this immediately partitions the model across all gpus, to avoid the overhead in time
             # and memory copying it on CPU or each GPU first
+            print("deepspeed_config()",deepspeed_config(),"config",config,"kwargs",kwargs)
             with deepspeed.zero.Init(config_dict_or_path=deepspeed_config()):
                 model = cls(config, **kwargs)
 
